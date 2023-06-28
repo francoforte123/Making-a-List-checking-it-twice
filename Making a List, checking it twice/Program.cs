1fdf78aa8747c88fa863fe53592bd2c9e460008b
rober <rober@DESKTOP-MN9AKPU>
2023-06-28 08:16:15 +0200

    Aggiungere i file di progetto.

---
+++ Making a List, checking it twice/Program.cs
@@ -0,0 +1,32 @@
+﻿Console.WriteLine("inserire 10 volte dei numeri:");
+int[] ArrayNumbers = new int[10];
+int i = 0;
+
+while (i < ArrayNumbers.Length)
+{
+    ArrayNumbers[i] = int.Parse(Console.ReadLine());
+    i++;
+}
+
+Console.WriteLine("------------------------------------------");
+Console.WriteLine("sto stampando l'elenco:");
+
+foreach (int number in ArrayNumbers)
+{
+    Console.WriteLine(number);
+}
+
+Console.WriteLine("------------------------------------------");
+Console.WriteLine("sto stampando l'elenco al contrario:");
+
+Array.Reverse(ArrayNumbers);
+foreach(int number in ArrayNumbers)
+{
+    Console.WriteLine(number);
+}
+
+
+
+
+
+
